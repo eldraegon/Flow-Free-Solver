1129455872fd1a32208f358ca8a6b6eb6b327924
eldraegon <50225910+eldraegon@users.noreply.github.com>
2020-10-26 23:08:25 -0400

    grid size change working
    now it works!

--- my-app/src/FlowFreeSolver/FlowFreeSolver.jsx
+++ my-app/src/FlowFreeSolver/FlowFreeSolver.jsx
@@ -2,45 +2,56 @@ import React, {Component} from 'react'
 import Node from './Node/Node'
 import './FlowFreeSolver.css'
 import { Button, ButtonGroup } from 'react-bootstrap';
-
+function test(num) {
+    console.log("TEST")
+}
 export default class FlowFreeSolver extends Component {
     constructor(props) {
         super(props);
         this.state = {
             nodes: [],
             length: 10,
+            size: 'lg',
         };
     }
 
     componentDidMount() {
+        this.createGrid(10);
+    }
+    
+    createGrid(length) {
         const nodes = []
-        for(let row = 0; row < this.state.length; row++) {
+        for(let row = 0; row < length; row++) {
             const currentRow = [];
-            for(let col = 0; col < this.state.length; col++) {
+            for(let col = 0; col < length; col++) {
                 const Node =  {
                     col,
                     row,
-                    isStart: row === 5 && col === 0,
-                    isFinish: row === 5 && col === 9,
+                    num: 0,
                 };
                 currentRow.push(Node);
             }
             nodes.push(currentRow);
         }
-        this.setState({nodes});
+        this.setState({nodes, length});
+    }
+
+    setGrid(length) {
+        if(length !== this.state.length) {
+            this.createGrid(length);
+        }
     }
 
     render() {
         const {nodes} = this.state;
         const navButtons = [];
         for(let i = 5; i <= 15; i++) {
-            navButtons.push(<Button className="Button">{i} x {i}</Button>);
+            navButtons.push(<Button key={i} variant="secondary" onClick={() => this.setGrid(i)}>{i}x{i}</Button>);
         }
-
         return (
             <div className="grid">
                 <div className="nav">
-                    <ButtonGroup>
+                    <ButtonGroup className="btn">
                         {navButtons}
                     </ButtonGroup>
                 </div>
@@ -49,11 +60,13 @@ export default class FlowFreeSolver extends Component {
                         <div key={rowIdx}>
                             {row.map((node, nodeIdx) => {
                                 const {isStart, isFinish} = node
+                                const {size} = this.state;
                                 return (
                                     <Node 
                                     key={nodeIdx}
                                     isStart={isStart}
-                                    isFinish={isFinish}>
+                                    isFinish={isFinish}
+                                    size={size}>
                                     </Node>
                                 );
                             })}
