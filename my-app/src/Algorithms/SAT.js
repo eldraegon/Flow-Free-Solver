e2b2077fd4a267c968a0d2d7a70e280d713f8d65
eldraegon <50225910+eldraegon@users.noreply.github.com>
2020-11-12 12:39:31 -0500

    Update SAT.js

--- my-app/src/Algorithms/SAT.js
+++ my-app/src/Algorithms/SAT.js
@@ -7,22 +7,13 @@ export default class SAT {
         this.solutionLength = length * length * numColors * 6;
         this.solution = []
         for(let i = 0; i < this.solutionLength; i++) this.solution.push(undefined);
-        this.sortCnf();
+        this.sort();
         this.solve();
     }
 
-    sortCnf() {
+    sort() {
         this.cnf.sort(function(a, b) {
             return a.length() - b.length();
         });
-    }
-    solveTrivialClauses() {
-        const {cnf, solutionLength, solution} = this;
-        cnf.forEach(element => {
-            if(element.length === 1) solution[element[0]] = element[0] < 0? false : true;
-        });
-    }
-    solve() {
-        let last = this.solveTrivialClauses();
     }
 }
